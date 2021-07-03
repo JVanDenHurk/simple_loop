172c121001b236999700ce7972bb57824558f966
Justin Van Den Hurk <Justin.t.hurk@gmail.com>
2021-07-04 01:32:03 +0930

    simple_loop

--- simple_loop.py
+++ simple_loop.py
@@ -1,17 +1,14 @@
-user_input = input("> ")
+user_input = ""
 
-while True:
+while user_input.lower() != "quit":
+    user_input = input("> ")
     if user_input.lower() == "help":
         print(f"start - to start the car \nstop - to stop the car \nquit - to exit \n")
-        user_input = input("> ")
     elif user_input.lower() == "start":
         print(f"Car started...Ready to go! \n")
-        user_input = input("> ")
     elif user_input.lower() == "stop":
         print(f"Car  Stopped.\n")
-        user_input = input("> ")
-    elif user_input.lower() == "quit":
-        break
+    elif user_input == "quit":
+        print("")
     else:
         print("I don't understand that...\n")
-        user_input = input("> ")
